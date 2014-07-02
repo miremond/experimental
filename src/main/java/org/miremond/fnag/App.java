ed7c27688626691f07329c753ae4391ce830423e
Michael Remond <michaelremond@gmail.com>
2014-07-02 09:52:15 +0200

    init repo

---
+++ src/main/java/org/miremond/fnag/App.java
@@ -0,0 +1,13 @@
+package org.miremond.fnag;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
